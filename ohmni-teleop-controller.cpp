04205d2f0d04060512265e828bb0fb75cc4caa2a
Hoang Nguyen <hoang@ohmnilabs.com>
2018-07-27 13:10:46 -0700

    ready to be tested on Intel NUC

--- ohmni-teleop-controller.cpp
+++ ohmni-teleop-controller.cpp
@@ -18,6 +18,30 @@ int main (int argc, char **argv) {
     ros::init(argc, argv, "ohmni-teleop/controller");
     ros::NodeHandle n;
 
-    // Now we want to publish our messages to our robot
-    ros::Publisher input_pub = n.advertise<std_msgs::Char>("ohmni-bot", 1000);
+    // User input prompt
+    std::cout << """
+        Control Ohmni!
+        ---------------------------
+        Moving around:
+                w    
+           a    s    d
+        r and f : looking up and down with neck
+        CTRL-C to quit
+    """
+    // Setting up publishing topic to Ohmni
+    // ros::Publisher input_pub = n.advertise<std_msgs::Char>("ohmni-bot", 1000);
+
+    // Storage space for user input key
+    std::char userKey;
+
+    while(1) {
+        // Getting user input
+        std::getline(std::cin, userKey);
+        std::cout << "Your input key is " << userKey << " \n";
+    }
+
+
+
+    // No errors
+    return 0;
 }
