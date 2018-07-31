3d0d9793bcb0baae085fe821c7daec95d51b6016
Hoang Nguyen <hoang@ohmnilabs.com>
2018-07-31 10:24:42 -0700

    fixed compiler errors due to typos and pointer reference/dereference. Modulated the code and added appropriate comments. Code is now compiled against catkin_make and can be run under rosrun, with only the Unix domain socket file not setup properly, gonna test this on the bot now

--- bot.cpp
+++ bot.cpp
@@ -13,10 +13,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <unistd.h> // for socket general
+#include <unistd.h> // for socket in general
 
 // Unix domain socket name, hardcoded for now
-#define NAME "data/data/com.ohmnilabs.telebot_rtc/files/node.sock"
+#define NAME "/opt/sockets/node.sock"
 
 // Macro for consistent buffer size
 #define BUFFER_SIZE 8192
@@ -26,19 +26,46 @@
 #include "std_msgs/String.h"
 
 // Global struct so we can store socket information and such
-stuct RosSock {
-    int* sock;
-    char buf[BUFFER_SIZE];
-    struct sockaddr_un server;
+struct RosSock {
+    int* sock; // pointer to the socket
+    char buf[BUFFER_SIZE]; // message buffer side
+    struct sockaddr_un server; // server configuration
 };
-struct RosSock _sock = {0};
+struct RosSock _sock = {0}; // init struct to zeros now
+
+// Unix domain client side initialize
+int ud_client_init() {
+
+     // Welp, create a socket first
+    int client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (client_socket < 0) {
+        ROS_ERROR("UD: opening stream socket error");
+        return -1;
+    }
+    _sock.sock = &client_socket;
+
+    // Configure the local file name so we can request the server
+    _sock.server.sun_family = AF_UNIX;
+    strcpy(_sock.server.sun_path, NAME);
+
+    // Sanity check for connection error
+    if (connect(*(_sock.sock), (struct sockaddr *) &(_sock.server), sizeof(struct sockaddr_un)) < 0) {
+        close(*(_sock.sock));
+        ROS_ERROR("UD: connecting stream socket error");
+        return -1;
+    }   
+}
 
 // Callback function on subscription
 void keyCB(const std_msgs::String::ConstPtr& msg) {
-    _sock.buf = msg->data.c_str();
+
+    // Store the message into our buffer
+    strcpy(_sock.buf, msg->data.c_str());
+    
+    // Declare for debug purpose now and write to socket
     ROS_INFO("Recieved: %s", _sock.buf);
-    if (write(_sock.&sock, _sock.buf, sizeof(_sock.buf)) < 0) {
-        perror("writing on stream socket");
+    if (write(*(_sock.sock), _sock.buf, sizeof(_sock.buf)) < 0) {
+        perror("UD: writing on stream socket error");
     }
 }
 
@@ -49,30 +76,11 @@ int main(int argc, char **argv) {
     // environment variables setup
     ros::init(argc, argv, "ohmni_bot");
     
-    /* UNIX domain client side setup here */
-    // Storage variables
-    // int sock;
-    // struct sockaddr_un server;
-    // char buf[BUFFER_SIZE];
-
-    // Welp, create a socket first
-    _sock.sock = &socket(AF_UNIX, SOCK_STREAM, 0);
-    if (_sock.&sock < 0) {
-        ROS_ERROR("opening stream socket");
+    /* UNIX domain client side setup here with sanity check */
+    if (ud_client_init() < 0) {
+        ROS_ERROR("UD: failed to establish Unix domain client");
         return -1;
     }
-
-    // Configure the local file name so we can request the server
-    _sock.server.sun_family = AF_UNIX;
-    strcpy(_sock.server.sun_path, NAME);
-
-    // Sanity check for connection error
-    if (connect(_sock.&sock, (struct sockaddr *) &(_sock.server), sizeof(struct sockaddr_un)) < 0) {
-        close(_sock.&sock);
-        ROS_ERROR("connecting stream socket");
-        return -1;
-    }
-
     // Subscibe to topic here
     ros::NodeHandle nh;
     ros::Subscriber teleop_key = nh.subscribe("ohmni_teleop_controller", 1000, keyCB);
